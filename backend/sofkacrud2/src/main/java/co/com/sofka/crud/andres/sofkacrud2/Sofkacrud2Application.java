697a9cbb262eb86631c6d98ee00164a5fdc620ac
Andres <andres.castaneda280040@gmail.com>
2022-05-15 13:32:19 -0500

    mi primer commit

---
+++ backend/sofkacrud2/src/main/java/co/com/sofka/crud/andres/sofkacrud2/Sofkacrud2Application.java
@@ -0,0 +1,13 @@
+package co.com.sofka.crud.andres.sofkacrud2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sofkacrud2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sofkacrud2Application.class, args);
+	}
+
+}
---
+++ backend/sofkacrud2/src/test/java/co/com/sofka/crud/andres/sofkacrud2/Sofkacrud2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.sofka.crud.andres.sofkacrud2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sofkacrud2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
